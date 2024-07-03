8c73525b1ca5b4f4af956c71c86dd2d98c56c974
Jared Hanson <jaredhanson@gmail.com>
2024-07-03 14:39:53 -0700

    Add tests.

--- test/handlers/challenge.test.js
+++ test/handlers/challenge.test.js
@@ -182,6 +182,141 @@ describe('handlers/challenge', function() {
         .listen();
     }); // should generate challenge for registration with username in preference to email
     
+    it('should generate challenge for registration with given name', function(done) {
+      var store = new Object();
+      store.challenge = sinon.stub().yieldsAsync(null, Buffer.from([21, 31, 105]));
+      
+      var handler = factory(store);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.method = 'POST';
+          req.body = {
+            type: 'webauthn.create',
+            username: 'alexm',
+            given_name: 'Alex'
+          }
+        })
+        .finish(function() {
+          expect(store.challenge).to.have.been.calledOnceWith(this.req);
+          var ctx = store.challenge.getCall(0).args[1];
+          expect(ctx.user.id).to.be.an.instanceOf(Buffer);
+          var handle = ctx.user.id;
+          delete ctx.user.id;
+          expect(ctx).to.deep.equal({
+            user: {
+              name: 'alexm',
+              displayName: 'Alex'
+            }
+          });
+          
+          expect(this.statusCode).to.equal(200);
+          expect(this.getHeader('Content-Type')).to.equal('application/json');
+          expect(this.body.challenge).to.equal('FR9p');
+          var user = this.body.user;
+          expect(user.id).to.be.a('string');
+          expect(user.id).to.equal(base64url.encode(handle));
+          delete user.id;
+          expect(user).to.deep.equal({
+            name: 'alexm',
+            displayName: 'Alex'
+          });
+          done();
+        })
+        .listen();
+    }); // should generate challenge for registration with given name
+    
+    it('should generate challenge for registration with given name and family name', function(done) {
+      var store = new Object();
+      store.challenge = sinon.stub().yieldsAsync(null, Buffer.from([21, 31, 105]));
+      
+      var handler = factory(store);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.method = 'POST';
+          req.body = {
+            type: 'webauthn.create',
+            username: 'alexm',
+            given_name: 'Alex',
+            family_name: 'Müller'
+          }
+        })
+        .finish(function() {
+          expect(store.challenge).to.have.been.calledOnceWith(this.req);
+          var ctx = store.challenge.getCall(0).args[1];
+          expect(ctx.user.id).to.be.an.instanceOf(Buffer);
+          var handle = ctx.user.id;
+          delete ctx.user.id;
+          expect(ctx).to.deep.equal({
+            user: {
+              name: 'alexm',
+              displayName: 'Alex Müller'
+            }
+          });
+          
+          expect(this.statusCode).to.equal(200);
+          expect(this.getHeader('Content-Type')).to.equal('application/json');
+          expect(this.body.challenge).to.equal('FR9p');
+          var user = this.body.user;
+          expect(user.id).to.be.a('string');
+          expect(user.id).to.equal(base64url.encode(handle));
+          delete user.id;
+          expect(user).to.deep.equal({
+            name: 'alexm',
+            displayName: 'Alex Müller'
+          });
+          done();
+        })
+        .listen();
+    }); // should generate challenge for registration with given name and family name
+    
+    it('should generate challenge for registration with name in preferece to given name and family name', function(done) {
+      var store = new Object();
+      store.challenge = sinon.stub().yieldsAsync(null, Buffer.from([21, 31, 105]));
+      
+      var handler = factory(store);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.method = 'POST';
+          req.body = {
+            type: 'webauthn.create',
+            username: 'alexm',
+            name: 'Alex M.',
+            given_name: 'Alex',
+            family_name: 'Müller'
+          }
+        })
+        .finish(function() {
+          expect(store.challenge).to.have.been.calledOnceWith(this.req);
+          var ctx = store.challenge.getCall(0).args[1];
+          expect(ctx.user.id).to.be.an.instanceOf(Buffer);
+          var handle = ctx.user.id;
+          delete ctx.user.id;
+          expect(ctx).to.deep.equal({
+            user: {
+              name: 'alexm',
+              displayName: 'Alex M.'
+            }
+          });
+          
+          expect(this.statusCode).to.equal(200);
+          expect(this.getHeader('Content-Type')).to.equal('application/json');
+          expect(this.body.challenge).to.equal('FR9p');
+          var user = this.body.user;
+          expect(user.id).to.be.a('string');
+          expect(user.id).to.equal(base64url.encode(handle));
+          delete user.id;
+          expect(user).to.deep.equal({
+            name: 'alexm',
+            displayName: 'Alex M.'
+          });
+          done();
+        })
+        .listen();
+    }); // should generate challenge for registration with name in preferece to given name and family name
+    
   }); // handler
   
 });
