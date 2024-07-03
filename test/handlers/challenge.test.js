618d5ebbfa86993bf807e98b3a35238e5057c308
Jared Hanson <jaredhanson@gmail.com>
2024-07-03 10:58:48 -0700

    Add tests.

--- test/handlers/challenge.test.js
+++ test/handlers/challenge.test.js
@@ -22,4 +22,32 @@ describe('handlers/challenge', function() {
     expect(bodyParserSpy).to.be.calledOnce;
   });
   
+  describe('handler', function() {
+    
+    it('should generate challenge', function(done) {
+      var store = new Object();
+      store.challenge = sinon.stub().yieldsAsync(null, Buffer.from([21, 31, 105]))
+
+
+      var handler = factory(store);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.method = 'POST';
+        })
+        .finish(function() {
+          expect(store.challenge).to.have.been.calledOnceWith(this.req);
+          
+          expect(this.statusCode).to.equal(200);
+          expect(this.getHeader('Content-Type')).to.equal('application/json');
+          expect(this.body).to.deep.equal({
+            challenge: 'FR9p'
+          })
+          done();
+        })
+        .listen();
+    });
+    
+  });
+  
 });
