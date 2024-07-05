6693d05f1198e2cdedad7dd45931436990c0254d
Jared Hanson <jaredhanson@gmail.com>
2024-07-05 13:22:52 -0700

    Add TODO item.

--- com/challengestore.js
+++ com/challengestore.js
@@ -1,6 +1,7 @@
 var SessionChallengeStore = require('passport-fido2-webauthn').SessionChallengeStore;
 
 exports = module.exports = function() {
+  // TODO: replace this with a state-based store for URL checking
   return new SessionChallengeStore();
 };
 
